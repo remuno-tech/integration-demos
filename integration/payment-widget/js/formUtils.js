e62549a999826b9f3f700270f4868d84169085ed
karpushyn <karpushyn@sednor.net>
2023-04-13 15:56:39 +0300

    refactor js demo

--- integration/payment-widget/js/formUtils.js
+++ integration/payment-widget/js/formUtils.js
@@ -1,90 +1,57 @@
 const BUTTON_SIZES = {
   selector: '#buttonSize',
-  options: ['small', 'medium', 'large']
+  options: ['small', 'medium', 'large'],
 };
 const WIDGET_THEME = {
   selector: '#theme',
-  options: ['light', 'dark']
+  options: ['light', 'dark'],
 };
 const WIDGET_VARIANT = {
   selector: '#variant',
-  options: ['dialog', 'popup', 'inline']
+  options: ['dialog', 'popup', 'inline'],
 };
 const QUOTE_EXPIRE_ANIMATION = {
   selector: '#quoteExpireAnimation',
-  options: ['skew', 'throb', 'shake']
+  options: ['skew', 'throb', 'shake'],
 };
 const FIAT_CURRENCY = {
   selector: '#fromFiat',
-  options: ['USD', 'EUR', 'GBP']
+  options: ['USD', 'EUR', 'GBP'],
 };
-
-const DEFAULT_SUMMARY = {
-  template:
-    'You will get:\\nAt least {d:decimalAmount} coins {i:coinImage:20px#20px}', // using \\n to display \n in input
-  variables: {
-    decimalAmount: '10000',
-    coinImage: 'https://remuno.com/icons/icon-placeholder.png',
-  },
-};
-
-//initial state
-let widgetSettings = {
-  apiKey: '',
-  merchantId: '',
-  buttonSize: 'small',
-  fromFiat: 'USD',
-  amount: 1,
-  variant: 'dialog',
-  orderId:
-    'Order' +
-    Math.floor(Math.random() * 10) +
-    Math.floor(Math.random() * 10) +
-    Math.floor(Math.random() * 10) +
-    Math.floor(Math.random() * 10),
-  orderDescription: '',
-  theme: 'light',
-  quoteExpireAnimation: 'skew',
-  showCoinProtocolBanner: true,
-  notificationUrl: '',
-  scriptHtml: '',
-  buttonHtml: '',
-  confirmCoinSelection: false,
-  marketRate: true,
-  isEnabledCustomDetails: false,
-  customDetails: '{}',
-  selectedCoins: [],
-  isEnabledSummary: false,
-  isSelectedCoinsEnabled: false,
-  summary: DEFAULT_SUMMARY,
-};
-
 
 // prefilling select components
-[BUTTON_SIZES, WIDGET_THEME, WIDGET_VARIANT, QUOTE_EXPIRE_ANIMATION, FIAT_CURRENCY].forEach(s => {
+[
+  BUTTON_SIZES,
+  WIDGET_THEME,
+  WIDGET_VARIANT,
+  QUOTE_EXPIRE_ANIMATION,
+  FIAT_CURRENCY,
+].forEach((s) => {
   const selector = document.querySelector(s.selector);
   if (selector) {
-    s.options.forEach(opt => {
+    s.options.forEach((opt) => {
       const option = document.createElement('option');
       option.value = opt;
       option.text = opt;
       selector.appendChild(option);
-    })
+    });
   }
-})
+});
 
 // setting default values from the config
-Object.keys(widgetSettings).forEach((field) => {
+Object.keys(remunoConfig).forEach((field) => {
   const selector = document.getElementById(field);
   if (selector) {
-    if (typeof widgetSettings[field] !== 'boolean') {
-      selector.value = widgetSettings[field];
+    if (typeof remunoConfig[field] === 'boolean') {
+      selector.checked = remunoConfig[field];
+    } else if (typeof remunoConfig[field] === 'object') {
+      selector.value = JSON.stringify(remunoConfig[field]);
     } else {
-      selector.checked = widgetSettings[field];
+      selector.value = remunoConfig[field];
     }
   }
-})
-setInitialSummary()
+});
+setInitialSummary();
 
 // check is valid json
 function stringToJson(str) {
@@ -126,36 +93,56 @@ function formatBreakline(string) {
 // set initial custom message on quote template input value
 function setInitialSummary() {
   const selector = document.getElementById('summaryTemplate');
-  selector.value = widgetSettings.summary.template;
-  parseTemplate(widgetSettings.summary.template);
+  selector.value = remunoConfig.summary.template;
+  parseTemplate(remunoConfig.summary.template);
 }
 
 // setter for coins
 function handleSelectCoin(coin) {
-  const isInList = widgetSettings.selectedCoins.includes(coin);
+  const isInList = remunoConfig.selectedCoins.includes(coin);
   if (isInList) {
-    widgetSettings.selectedCoins = widgetSettings.selectedCoins.filter(c => c !== coin);
+    remunoConfig.selectedCoins = remunoConfig.selectedCoins.filter(
+      (c) => c !== coin,
+    );
   } else {
-    widgetSettings.selectedCoins.push(coin)
+    remunoConfig.selectedCoins.push(coin);
   }
-  handleUpdateWidget();
+  updateWidget();
 }
 
 // get coins and set options to choose
 function setCoinsList() {
   const selector = document.getElementById('selectedCoinsContainer');
-  fetch(`https://api.remuno.com/v1/merchants/${widgetSettings.merchantId}/coins`, { headers: { 'x-api-key': widgetSettings.apiKey } })
-    .then(res => res.json()).then(data => {
-    selector.innerHTML = '';
-    data?.forEach(coin => {
-      const input = `<div>
-<label class="form-check-label" for="coins-${coin.displayName}">${coin.displayName}</label>
-     <input className="form-check-input" onchange="handleSelectCoin('${coin.code}', this.checked)" type="checkbox"
-           role="switch" id="coins-${coin.displayName}">
-</div>`;
-      selector.innerHTML += input;
-    })
-  });
+  fetch(
+    `https://api.remuno.com/v1/merchants/${remunoConfig.merchantId}/coins`,
+    { headers: { 'x-api-key': remunoConfig.apiKey } },
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      selector.innerHTML = '';
+      data?.forEach((coin) => {
+        const input = `<div>
+             <label class="form-check-label" for="coins-${coin.displayName}">${coin.displayName}</label>
+             <input className="form-check-input" onchange="handleSelectCoin('${coin.code}', this.checked)" type="checkbox"
+                    role="switch" id="coins-${coin.displayName}">
+          </div>`;
+        selector.innerHTML += input;
+      });
+    });
+}
+
+// setter for custom message on quote template
+function handleChangeSummaryTemplate(value) {
+  remunoConfig.summary.template = value;
+  parseTemplate(value);
+  updateWidget();
+}
+
+// setter for custom message on quote variables
+function handleSummaryVariableChange(field, value) {
+  console.log(remunoConfig.summary);
+  remunoConfig.summary.variables[field] = value;
+  updateWidget();
 }
 
 // parse custom message on quote template to create input for each variable
@@ -164,54 +151,35 @@ function parseTemplate(template) {
     const elements = template.match(/{[a-z]*:[^\s}]+}|{[^\s}]+}/g);
     const formattedElements = elements?.map((el) => {
       const result = /{([^}]*)}/.exec(el);
-      const [type, name] = (result)[1].split(':');
+      const [type, name] = result[1].split(':');
       return formatElement(type, name || type);
     });
-    const fields = formattedElements.map(el => el.name);
+    const fields = formattedElements.map((el) => el.name);
     const container = document.getElementById('summaryVariables');
     container.innerHTML = '';
-    formattedElements.forEach(el => {
-      const input = `
-            <label class="form-check-label" for="summary-${el.name}">${el.name}, type:${el.type}</label>
+    formattedElements.forEach((el) => {
+      const input = `<label class="form-check-label" for="summary-${el.name}">${
+        el.name
+      }, type: ${el.type}</label>
           <input 
             type="text" 
             class="form-control"
-            value="${widgetSettings.summary.variables[el.name] || ''}"
-            onchange="handleSummaryVariableChange(${el.name}, this.value)"
-            id="summary-${el.name}">`
+            value="${remunoConfig.summary.variables[el.name] || ''}"
+            onchange="handleSummaryVariableChange('${el.name}', this.value)"
+            id="summary-${el.name}">`;
       container.innerHTML += input;
     });
-    widgetSettings.summary.variables = Object.fromEntries(Object.entries(widgetSettings.summary.variables)
-      .filter(([key]) => fields.includes(key)));
+    remunoConfig.summary.variables = Object.fromEntries(
+      Object.entries(remunoConfig.summary.variables).filter(([key]) =>
+        fields.includes(key),
+      ),
+    );
   }
-  return [];
 }
 
-// update widget with updated values
-function handleUpdateWidget() {
-  window.Remuno.Widget.updateConfig({
-    selector: '#widget-preview',
-    customDetails: widgetSettings.isEnabledCustomDetails
-      ? stringToJson(widgetSettings.customDetails)
-      : undefined,
-    summary: widgetSettings.isEnabledSummary ? {
-      ...widgetSettings.summary,
-      template: formatBreakline(widgetSettings.summary.template),
-    } : undefined,
-    selectedCoins: widgetSettings.isSelectedCoinsEnabled ? widgetSettings.selectedCoins : undefined,
-    ...widgetSettings
-  });
-}
-
-// setter for custom message on quote template
-function handleChangeSummaryTemplate(value) {
-  widgetSettings.summary.template = value;
-  parseTemplate(value);
-}
-
-// setter for custom message on quote variables
-function handleSummaryVariableChange(field, value) {
-  widgetSettings.summary.variables[field] = value;
+function handleCloseModal() {
+  document.querySelector('.modal').classList.remove('show');
+  document.querySelector('.modal').style.display = 'none';
 }
 
 // setter for all values
@@ -219,9 +187,10 @@ function handleChange(field, value) {
   switch (field) {
     case 'apiKey':
     case 'merchantId':
-      widgetSettings[field] = value;
-      if (widgetSettings.apiKey && widgetSettings.merchantId) {
-        setCoinsList()
+      localStorage.setItem(field, value);
+      remunoConfig[field] = value;
+      if (remunoConfig.apiKey && remunoConfig.merchantId) {
+        setCoinsList();
       }
       break;
     case 'isEnabledCustomDetails':
@@ -243,7 +212,7 @@ function handleChange(field, value) {
       }
       break;
     default:
-      widgetSettings[field] = value;
+      remunoConfig[field] = value;
   }
-  handleUpdateWidget();
+  updateWidget();
 }
--- integration/payment-widget/js/widgetConfig.js
+++ integration/payment-widget/js/widgetConfig.js
@@ -1,26 +1,92 @@
-window.Remuno = {
-  Widget: {
-    onLoad: function () {
-      var callbacks = {
-        onTransactionCompleted: (data) =>
-          console.log('TXN COMPLETED SUCCESS', data),
-        onTransactionExpired: (data) =>
-          console.log('TXN COMPLETED FAIL', data),
-        onTransactionCreated: (data) =>
-          console.log('TXN CREATED SUCCESS', data),
-        onTransactionFailed: (data) =>
-          console.log('TXN CREATED FAIL', data),
-        onQuoteUpdated: (data) => console.log('QUOTE UPDATED', data),
-        onTransactionCancelled: (data) =>
-          console.log(`TXN CANCELLED`, data),
-        onClose: () => console.log('WIDGET CLOSED'),
-        onOpen: () => console.log('WIDGET OPENED'),
-      };
+const callbacks = {
+  onTransactionCompleted: (data) => {
+    document.querySelector('.modal').classList.add('show');
+    document.querySelector('.modal').style.display = 'block';
+    document.querySelector('#order-id').innerHTML = data.orderId;
+    console.log('TXN COMPLETED SUCCESS', data);
+  },
+  onTransactionExpired: (data) => console.log('TXN COMPLETED FAIL', data),
+  onTransactionCreated: (data) => console.log('TXN CREATED SUCCESS', data),
+  onTransactionFailed: (data) => console.log('TXN CREATED FAIL', data),
+  onQuoteUpdated: (data) => console.log('QUOTE UPDATED', data),
+  onTransactionCancelled: (data) => console.log(`TXN CANCELLED`, data),
+  onClose: () => console.log('WIDGET CLOSED'),
+  onOpen: () => console.log('WIDGET OPENED'),
+};
 
-      window.Remuno.Widget.init({
-        selector: '#widget-preview',
-        callbacks,
-      });
-    },
+const apiKey = localStorage.getItem('apiKey');
+const merchantId = localStorage.getItem('merchantId');
+
+let DEFAULT_SUMMARY = {
+  template:
+    'You will get:\\nAt least {d:decimalAmount} coins {i:coinImage:20px#20px}', // using \\n to display \n in input
+  variables: {
+    decimalAmount: '10000',
+    coinImage: 'https://remuno.com/icons/icon-placeholder.png',
   },
 };
+
+let remunoConfig = {
+  selector: '#widget-preview',
+  apiKey: apiKey || '',
+  merchantId: merchantId || '',
+  buttonSize: 'small',
+  fromFiat: 'USD',
+  amount: 1,
+  variant: 'dialog',
+  orderId:
+    'Order' +
+    Math.floor(Math.random() * 10) +
+    Math.floor(Math.random() * 10) +
+    Math.floor(Math.random() * 10) +
+    Math.floor(Math.random() * 10),
+  orderDescription: '',
+  theme: 'light',
+  quoteExpireAnimation: 'skew',
+  showCoinProtocolBanner: true,
+  notificationUrl: '',
+  scriptHtml: '',
+  buttonHtml: '',
+  confirmCoinSelection: false,
+  marketRate: true,
+  isEnabledCustomDetails: false,
+  customDetails: {},
+  selectedCoins: [],
+  isEnabledSummary: false,
+  isSelectedCoinsEnabled: false,
+  summary: DEFAULT_SUMMARY,
+  callbacks,
+};
+
+if (window.Remuno && window.Remuno.Widget) {
+  window.Remuno.Widget.init(remunoConfig);
+} else {
+  window.Remuno = {
+    Widget: {
+      onLoad: function () {
+        window.Remuno.Widget.init(remunoConfig);
+      },
+    },
+  };
+}
+
+// update widget with the latest values from the config. Can be used as patch (update only one or multiple params)
+// For example: window.Remuno.Widget.updateConfig({ amount: 2 });
+function updateWidget() {
+  window.Remuno.Widget.updateConfig({
+    selector: '#widget-preview',
+    customDetails: remunoConfig.isEnabledCustomDetails
+      ? stringToJson(remunoConfig.customDetails)
+      : undefined,
+    summary: remunoConfig.isEnabledSummary
+      ? {
+          ...remunoConfig.summary,
+          template: formatBreakline(remunoConfig.summary.template),
+        }
+      : undefined,
+    selectedCoins: remunoConfig.isSelectedCoinsEnabled
+      ? remunoConfig.selectedCoins
+      : undefined,
+    ...remunoConfig,
+  });
+}
