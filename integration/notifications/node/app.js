59e1c81762c68015f47dfdc3ab04a84b88ae6d49
FusionBuild <FusionBuild@fastmarkets.com>
2023-04-25 11:50:47 +0100

    Add listener, and return 200 code to acknowledge success.

--- integration/notifications/node/app.js
+++ integration/notifications/node/app.js
@@ -12,8 +12,12 @@ app.post('/notification-test', (req, res) => {
   const notificationKey = ''; // enter your notification key here
   const signatureKey = req.headers['x-remuno-sig'];
   const isValid = isValidSignature(req.body, notificationKey, signatureKey);
-  console.log('Signature is valid: ', isValid)
-  res.send({ isValid })
+  console.log('Signature is valid: ', isValid);
+  res.sendStatus(200);
+})
+
+app.listen(80, () => {
+  console.log('Listening on port 80!');
 })
 
 module.exports = app;
